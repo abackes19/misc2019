11bd50fada5e9674253dd7bdc74a5ce4f6d5c667
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-07-04 05:33:52 -0500

    Update Camera_swivel.py

--- Camera_swivel.py
+++ Camera_swivel.py
@@ -13,17 +13,20 @@ key_down = time.time()
 while key != ord('q'):
     key = screen.getch()
     screen.clear()
+    screen.addstr(0, 0, 'Hit "q" to quit, and "a" or "d" to turn')
+    screen.addstr(1, 0, 'Current movement state:')
+
     if key == ord('a'):
         RPL.servoWrite(motor_pin, 1000)
         key_down = time.time()
-        screen.addstr('clockwise')
+        screen.addstr(1, 24, 'Counterclockwise')
     if key == ord('d'):
         RPL.servoWrite(motor_pin, 2000)
         key_down = time.time()
-        screen.addstr('counterclockwise')
+        screen.addstr(1, 24, 'Clockwise')
     if time.time() - key_down > 0.5:
         RPL.servoWrite(motor_pin, 0)
-        screen.addstr('stopped')
+        screen.addstr(1, 24, 'Stopped')
     if key == ord('q'):
         RPL.servoWrite(motor_pin, 0)
         curses.endwin()
